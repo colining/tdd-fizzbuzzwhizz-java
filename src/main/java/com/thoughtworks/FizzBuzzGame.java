e4fe5850bf8fa0becaff0aecd0e8e3f0162bee2f
colining <18810782954@163.com>
2018-04-11 14:20:43 +0800

    complete tdd practice

--- src/main/java/com/thoughtworks/FizzBuzzGame.java
+++ src/main/java/com/thoughtworks/FizzBuzzGame.java
@@ -5,14 +5,63 @@ import java.util.List;
 
 
 public class FizzBuzzGame {
+
     private List<String> results = new ArrayList<>();
 
     public void start(int count) {
-
+        for (int i = 1; i <= count; i++) {
+            results.add(reportNumber(i));
+        }
     }
 
     public List<String> getResults() {
         return results;
+    }
+
+    String isHave3or5or7(int i) {
+        String string = String.valueOf(i);
+        char[] chars = string.toCharArray();
+        for (int j = 0; j < string.length(); j++) {
+            if (chars[j] == '3') {
+                return "Fizz";
+            }
+        }
+        return "";
+    }
+
+
+    String isMultiple3(int test) {
+        if (test % 3 == 0) {
+            return "Fizz";
+        }
+        return "";
+    }
+
+    String isMultiple5(int test) {
+        if (test % 5 == 0) {
+            return "Buzz";
+        }
+        return "";
+
+    }
+
+    String isMultiple7(int test) {
+        if (test % 7 == 0) {
+            return "Whizz";
+        }
+        return "";
+    }
+
+    String reportNumber(int test) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(isHave3or5or7(test));
+        if (stringBuilder.length() == 0) {
+            stringBuilder
+                    .append(isMultiple3(test))
+                    .append(isMultiple5(test))
+                    .append(isMultiple7(test));
+        }
+        return stringBuilder.toString().equals("") ? String.valueOf(test) : stringBuilder.toString();
     }
 }
 
--- src/test/java/com/thoughtworks/FizzBuzzGameTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzGameTest.java
@@ -1,10 +1,73 @@
 package com.thoughtworks;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class FizzBuzzGameTest {
+    private FizzBuzzGame fizzBuzzGame;
+
+    @Before
+    public void setUp() {
+        fizzBuzzGame = new FizzBuzzGame();
+    }
+
     @Test
     public void should_return_Fizz_when_have_3() {
+        int test = 13;
+        Assert.assertEquals("Fizz", fizzBuzzGame.isHave3or5or7(test));
+        test = 31;
+        Assert.assertEquals("Fizz", fizzBuzzGame.isHave3or5or7(test));
+        test = 25;
+        Assert.assertEquals("Buzz", fizzBuzzGame.isHave3or5or7(test));
+        test = 16;
+        Assert.assertEquals("", fizzBuzzGame.isHave3or5or7(test));
+    }
 
+    @Test
+    public void should_return_Fizz_when_is_multiple_3() {
+        int test = 3;
+        Assert.assertEquals("Fizz", fizzBuzzGame.isMultiple3(test));
+        test = 5;
+        Assert.assertEquals("", fizzBuzzGame.isMultiple3(test));
+    }
+    @Test
+    public void should_return_Buzz_when_is_multiple_5() {
+        int test = 5;
+        Assert.assertEquals("Buzz", fizzBuzzGame.isMultiple5(test));
+        test = 3;
+        Assert.assertEquals("", fizzBuzzGame.isMultiple5(test));
+    }
+    @Test
+    public void should_return_Whizz_when_is_multiple_7() {
+        int test = 7;
+        Assert.assertEquals("Whizz", fizzBuzzGame.isMultiple7(test));
+        test = 3;
+        Assert.assertEquals("", fizzBuzzGame.isMultiple7(test));
+    }
+
+    @Test
+    public void should_return_Fizz_Buzz_Whizz() {
+        int test = 210;
+        Assert.assertEquals("FizzBuzzWhizz",fizzBuzzGame.reportNumber(test));
+         test = 60;
+        Assert.assertEquals("FizzBuzz",fizzBuzzGame.reportNumber(test));
+        test = 140;
+        Assert.assertEquals("BuzzWhizz",fizzBuzzGame.reportNumber(test));
+        test = 21;
+        Assert.assertEquals("FizzWhizz",fizzBuzzGame.reportNumber(test));
+        test = 30;
+        Assert.assertEquals("Fizz",fizzBuzzGame.reportNumber(test));
+        test = 70;
+        Assert.assertEquals("Whizz",fizzBuzzGame.reportNumber(test));
+        test = 35;
+        Assert.assertEquals("Fizz",fizzBuzzGame.reportNumber(test));
+    }
+
+    @Test
+    public void should_return_100() {
+        int count = 100;
+        fizzBuzzGame.start(count);
+        Assert.assertEquals(100, fizzBuzzGame.getResults().size());
     }
 }
